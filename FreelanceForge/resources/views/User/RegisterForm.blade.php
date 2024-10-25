9afea89a56adfe1f00010cd3e19dbbb110bc5384
Roberto V. Ramirez Jr. <149548871+robram3000@users.noreply.github.com>
2024-10-25 20:34:35 +0800

    Changes

--- FreelanceForge/resources/views/User/RegisterForm.blade.php
+++ FreelanceForge/resources/views/User/RegisterForm.blade.php
@@ -4,91 +4,52 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-    {{-- title and icons  --}}
     <title>Register Account</title>
-    <link rel="icons" href="">
-
-
-    {{-- Control flow for identify the  --}}
-
+    <link rel="icon" href="">
+    @vite(['resources/scss/RegisterForm.scss'])
 </head>
 <body>
-      <main>
+    <div class="overlay"></div> <!-- Overlay image container -->
+    <main>    
         <form action="" method="POST" class="Form-container">
-            {{-- pangalan ng user na mag reregister --}}
+            <h1>Register Account</h1>
+            <div class="Container-content-inline">
+                <div class="Container-labelandtextbox-inline">
+                    <label for="">Firstname</label>
+                    <input type="text" name="Firstname" placeholder="Firstname" required>
+                </div>
+                <div class="Container-labelandtextbox-inline">
+                    <label for="">Lastname</label>
+                    <input type="text" name="Lastname" placeholder="Lastname" required>
+                </div>
+            </div>
+
+            <div class="Container-content-inline">
+                <div class="Container-labelandtextbox-inline">
+                    <label for="">Age</label>
+                    <input type="text" name="Age" placeholder="Age" required>
+                </div>
+                <div class="Container-labelandtextbox-inline">
+                    <label for="">Birthday</label>
+                    <input type="date" name="Birthday" required>
+                </div>
+            </div>
+
             <div class="Container-content">
                 <div class="Container-labelandtextbox">
-                    <label for="">Firstname</label>
-                    <input 
-                            type="text" 
-                            name="Firstname"
-                            placeholder="Firstname" 
-                            required>
-                </div>
-                <div class="Container-labelandtextbox">
-                    <label for="">Middlename</label>
-                    <input 
-                            type="text" 
-                            name="Middlename" 
-                            placeholder="Middlename" 
-                            required>
-                </div>
-                <div class="Container-labelandtextbox">
-                    <label for="">Lastname</label>
-                    <input 
-                            type="text" 
-                            name = "Lastname" 
-                            placeholder="Lastname" 
-                            required>
-                </div>
-                <div class="Container-labelandtextbox">
-                    <label for="">Suffix</label>
-                    <input 
-                            type="text" 
-                            name = "Suffix" 
-                            placeholder="Suffix eg(Sr, Jr)" >
-                </div class="Container-labelandtextbox">
-            </div>
-             {{-- Sensor Data that secure in database --}}    
-            <div>
-           
-                <div class="Container-labelandtextbox">
-                    <label for="">Age</label>
-                    <input 
-                            type="text" 
-                            name = "Age"
-                            placeholder="Age" 
-                            required>
-                </div >
-                <div class="Container-labelandtextbox">
-                    <label for="">Birthday</label>
-                    <input 
-                            type="date"
-                            name ="date" 
-                            required>
-                </div>
-                <div class="Container-labelandtextbox">
                     <label for="">Address</label>
-                    <input 
-                            type="text" 
-                            name = "Address"
-                            placeholder="Address" 
-                            required>
+                    <input type="text" name="Address" placeholder="Address" required>
                 </div>
                 <div class="Container-labelandtextbox">
                     <label for="">Phonenumber</label>
-                    <input 
-                            type="text" 
-                            name="Phonenumber"
-                            placeholder="Suffix eg(Sr, Jr)">
+                    <input type="text" name="Phonenumber" placeholder="Phonenumber">
                 </div>
             </div>
+
             <div class="Container-Button">
-                <Button type= "Submit">Submit</Button>
+                <button type="submit">Submit</button>
             </div>
         </form>         
-      </main>
-    
+    </main>
 </body>
 </html>
