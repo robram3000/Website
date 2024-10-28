477ce55249c8647be57ab5ace62b2e6b09ae0d97
Roberto V. Ramirez Jr. <149548871+robram3000@users.noreply.github.com>
2024-10-28 09:58:07 +0800

    PostController changes

--- FreelanceForge/resources/views/User/Layout/Head.blade.php
+++ FreelanceForge/resources/views/User/Layout/Head.blade.php
@@ -2,6 +2,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="Roberto V Ramirez Jr">
+    <meta name="keywords" content="web development, digital marketing, SEO, social media, online marketing">
+
     @vite(['resources/scss/Navigation.scss', 'resources/scss/Base.scss' , 'resources/scss/RegisterForm.scss' , 'resources/js/app.js','resources/scss/Roletype.scss'])
     <title>{{ $title }}</title>
+    <link rel="icon" href="{{ URL('images/FreelanceFrogeicons.png') }}" type="image/x-icon">
+
 </head>
